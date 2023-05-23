6b221d8758e2cff612880d8e7381b15b56266cb2
AKAVespaET3 <quattrini244@gmail.com>
2023-05-23 16:11:28 +0900

    2023-05-23

---
+++ src/MakingGameOneHand.java
@@ -0,0 +1,23 @@
+public class MakingGameOneHand {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        
+        System.out.println(a + " " + b + " " + c);
+
+        a = 0;
+        b = 1;
+        c = 2;
+        
+        System.out.println(a + " " + b + " " + c);
+        
+        a = 2;
+        b = 0;
+        c = 1;
+        
+        System.out.println(a + " " + b + " " + c);
+        
+       
+    }
+}
