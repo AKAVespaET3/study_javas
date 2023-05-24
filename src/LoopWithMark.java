5a6e3d199f27a095101440b44f96311a2b056696
AKAVespaET3 <quattrini244@gmail.com>
2023-05-24 15:21:50 +0900

    2023-05-24

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -1,8 +1,8 @@
 public class LoopWithMark {
     public static void main(String[] args) {
 
-        for(int i = 0 ; i < 5 ; i = i + 1 ){
-            System.out.println("i 값은" + i + "입니다.");
+        for(int i = 1 ; i < 5 ; i = i + 1 ){
+            System.out.println("For" + i + "입니다.");
             System.out.println();
         }
         
