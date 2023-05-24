b473ce201e5e9797c4f161e954147647360b6ea8
AKAVespaET3 <quattrini244@gmail.com>
2023-05-24 13:13:38 +0900

    2023-05-24

---
+++ src/PlusTwoVar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PlusTwoVar {
+    public static void main(String[] args) {
+        Scanner Goni = new Scanner(System.in);
+
+        int first = Goni.nextInt();
+        int second = Goni.nextInt();
+
+        System.out.println(first + second);
+    }
+    
+}
