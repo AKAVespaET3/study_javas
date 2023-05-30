7550b8e3cddd739ee1e585454a1b2d41dc5bf2be
AKAVespaET3 <quattrini244@gmail.com>
2023-05-30 13:16:09 +0900

    2023-05-30

--- src/MethodBasics.java
+++ src/MethodBasics.java
@@ -8,13 +8,21 @@ public class MethodBasics {
         int result = x + 1;
         return result;
     }
-    
+    public int T(int y){
+        int result = y + 1;
+        return result;
+    }
     public static void main(String[] args) {
         try {
             int x = 4;
             int y = 0;
-            y = x + 1;
-            y = y + 1; 
+            // y = x + 1; // 변수에 + 1
+            MethodBasics methodBacsic = new MethodBasics(); //new 다음 클래스() 괄호 넣어서 function화 클래스도 변수에 담을 수 있음(인스턴스화) 클래스도 타입으로 쓸 수 있음
+                                                            // new MethodBasics() 인스턴스화 function 리스트 볼 수 있음
+            y = methodBacsic.F(x);
+
+            y = methodBacsic.T(y);
+            y = y + 1; // 변수에 + 1
         } catch (Exception e) {
             
         }
--- src/Study.java
+++ src/Study.java
@@ -6,13 +6,12 @@ public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
     int A = in.nextInt();
-    int B = in.nextInt();
-
-    for(int C = 0; C < 6; C++ ){
-        System.out.println(A+B);
-        A = in.nextInt();
-        B = in.nextInt();      
-        in.close();  
+    int sum = 0;
+    
+    for(int i = 1 ;  i<= A; i++){
+        sum = sum + i;
     }
+    System.out.println(sum);
+    in.close();  
  }  
 }
